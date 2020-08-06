67242c0df82e43e9734bc714a69df77b15b4977a
Tagir <gtagir11@yandex.ru>
2020-08-06 15:26:26 +0500

    add killing LivingEntity and killing with arrows

--- src/ga/garifullin/stickkick/Handler.java
+++ src/ga/garifullin/stickkick/Handler.java
@@ -3,7 +3,10 @@ package ga.garifullin.stickkick;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
@@ -11,38 +14,57 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 public class Handler implements Listener {
     @EventHandler
     public void stickHit(EntityDamageByEntityEvent e){
-        if((e.getDamager() instanceof Player) && (e.getEntity() instanceof Player)){
-            Player admin = (Player) e.getDamager();
-            Player daun = (Player) e.getEntity();
-            String itemlore = ItemHandler.checkItemAndReturnLore(admin.getInventory().getItemInMainHand());
-            if( !(itemlore.equals("none")) && admin.hasPermission("ga.garifullin.stickkick.use")){
-                e.setCancelled(true);
-                switch (itemlore) {
-                    case Actions.KICK:
-                       daun.kickPlayer(ChatColor.RED + "You kicked!");
-                       admin.sendMessage(ChatColor.GOLD + daun.getName() + ChatColor.RED +" был кикнут");
-                       break;
-                    case Actions.KILL:
-                        daun.setHealth(0);
-                        admin.sendMessage(ChatColor.GOLD + daun.getName() + ChatColor.RED +" был убит");
-                        break;
-                    case Actions.SPECTATOR:
-                        daun.setGameMode(GameMode.SPECTATOR);
-                        admin.sendMessage(ChatColor.GOLD + daun.getName() + ChatColor.RED +" стал наблюдателем");
-                        break;
-                    case Actions.BAN:
-                        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "ban " + daun.getName() + " Ban((");
-                        admin.sendMessage(ChatColor.GOLD + daun.getName() + ChatColor.RED +" был забанен");
-                        break;
-                    case Actions.BANIP:
-                        String ip = daun.getAddress().getHostName();
-                        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "ban " + daun.getName() + " You got banned by IP((");
-                        Bukkit.banIP(ip);
-                        admin.sendMessage(ChatColor.GOLD + daun.getName() + ChatColor.RED +" был забанен");
-                        admin.sendMessage(ChatColor.GOLD + ip + ChatColor.RED +" IP адрес был забанен");
-                        break;
+        Entity damager = e.getDamager();
+        Entity entity = e.getEntity();
+
+        if ((damager instanceof Player) || ( (damager instanceof Projectile) && (((Projectile) damager).getShooter() instanceof Player) )) {
+            Player punisher = getPunisher(damager);
+
+            if (entity instanceof Player){
+                Player daun = (Player) entity;
+                String itemlore = ItemHandler.checkItemAndReturnLore(punisher.getInventory().getItemInMainHand());
+                if( !(itemlore.equals("none")) && punisher.hasPermission("ga.garifullin.stickkick.use")){
+                    e.setCancelled(true);
+                    switch (itemlore) {
+                        case Actions.KICK:
+                            daun.kickPlayer(ChatColor.RED + "You kicked!");
+                            punisher.sendMessage(ChatColor.GOLD + daun.getName() + ChatColor.RED +" был кикнут");
+                            break;
+                        case Actions.KILL:
+                            daun.setHealth(0);
+                            punisher.sendMessage(ChatColor.GOLD + daun.getName() + ChatColor.RED +" был убит");
+                            break;
+                        case Actions.SPECTATOR:
+                            daun.setGameMode(GameMode.SPECTATOR);
+                            punisher.sendMessage(ChatColor.GOLD + daun.getName() + ChatColor.RED +" стал наблюдателем");
+                            break;
+                        case Actions.BAN:
+                            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "ban " + daun.getName() + " Ban((");
+                            punisher.sendMessage(ChatColor.GOLD + daun.getName() + ChatColor.RED +" был забанен");
+                            break;
+                        case Actions.BANIP:
+                            String ip = daun.getAddress().getHostName();
+                            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "ban " + daun.getName() + " You got banned by IP((");
+                            Bukkit.banIP(ip);
+                            punisher.sendMessage(ChatColor.GOLD + daun.getName() + ChatColor.RED +" был забанен");
+                            punisher.sendMessage(ChatColor.GOLD + ip + ChatColor.RED +" IP адрес был забанен");
+                            break;
+                    }
+                }
+            } else if (entity instanceof LivingEntity){
+                if(ItemHandler.checkItemAndReturnLore(punisher.getInventory().getItemInMainHand()).equals("kill")) {
+                    e.setCancelled(true);
+                    ((LivingEntity) entity).setHealth(0);
                 }
             }
+        }
+    }
+
+    private static Player getPunisher(Entity damager){ // do not use!!! because code in else block may cause an error if it is not used correctly!!!! Error may be during casting to Player type
+        if (damager instanceof Player) {
+             return (Player) damager;
+        } else {
+            return (Player) ((Projectile) damager).getShooter();
         }
     }
 }
